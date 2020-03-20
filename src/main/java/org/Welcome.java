f7c1069b0a4f0d772b2b38644e20173fafaa581f
fenaye15 <62122793+fenaye15@users.noreply.github.com>
2020-03-19 20:16:39 -0400

    Update Welcome.java

--- src/main/java/org/Welcome.java
+++ src/main/java/org/Welcome.java
@@ -2,6 +2,6 @@ package org;
 public class Welcome {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Java Programming!");
+        System.out.println("Welcome to today's Java Programming!");
     }
 }
