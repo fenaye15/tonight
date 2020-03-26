de9a20702a61eeba13f12f50e5c0c78aabf9d1ed
fenaye15 <62122793+fenaye15@users.noreply.github.com>
2020-03-26 19:27:03 -0400

    Create Bike.java

---
+++ src/main/java/org/Bike.java
@@ -0,0 +1,19 @@
+package org;
+public class Bike {
+    private int size;
+    private String name;
+
+    public void ride() {
+        System.out.println("riding the " + name );
+    }
+
+
+        public static void main (String arg[]){
+            /////take a ride
+            Bike firstBike = new Bike();
+            firstBike.name = "road";
+            firstBike.size = 24;
+            firstBike.ride();
+        }
+
+    }
