4f6d4d283bb2b1d335b5b27a41bee12e5983ef3e
fenaye15 <62122793+fenaye15@users.noreply.github.com>
2020-04-02 20:22:30 -0400

    Create ClassAverage.java

---
+++ src/main/java/org/ClassAverage.java
@@ -0,0 +1,33 @@
+package org;
+// Fig. 4.8: ClassAverage.java
+import java.util.Scanner;
+
+public class ClassAverage {
+    public static void main(String[] args) {
+        // create scannerr
+        Scanner input = new Scanner(System.in);
+
+        //initialization phase
+        int total = 0;
+        int gradeCounter = 1;
+
+        //processing phase
+        while (gradeCounter <= 10) {  //loop 10 times
+            System.out.print("Enter grade: ");
+            int grade = input.nextInt();
+            total = total + grade;
+            gradeCounter = gradeCounter + 1;
+        }
+
+        //termination phase
+        int average = total / 10;
+
+        //display total and average of grades
+        System.out.printf("%nTotal of all 10 grades is %d%n", total);
+        System.out.printf("Class average is %d%n", average);
+        input();
+    }
+
+    private static void input() {
+    }
+}
