1d4cfa3397ab8d43c537a76a7c1834998b008034
fenaye15 <62122793+fenaye15@users.noreply.github.com>
2020-04-02 20:42:50 -0400

    Create VotingAge.java

---
+++ src/main/java/org/VotingAge.java
@@ -0,0 +1,14 @@
+package org;
+
+public class VotingAge {
+
+    public static void main(String... argvs) {
+        //String a= "Che";
+        int age = 18;
+        if (age < 18) {
+            System.out.println ("%n%sYou cannot vote");
+        } else {
+            System.out.println ("You can vote");
+        }
+        }
+    }
