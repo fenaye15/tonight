6385c0bd6cdab1953ade6152816c21f03287d7d4
fenaye15 <62122793+fenaye15@users.noreply.github.com>
2020-04-16 19:14:34 -0400

    Create Power.java

---
+++ src/main/java/org/Power.java
@@ -0,0 +1,10 @@
+package org;
+//Exercise 3.5.3 Part2: Power.java
+
+public class Power {
+    public static void main (String[] args) {
+
+    double result = Math.pow(2.5, 3);
+    System.out.printf("%n%d ", result);
+    }
+}
