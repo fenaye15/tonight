e4b362e70a0abb9e1b889556805493b19f804168
fenaye15 <62122793+fenaye15@users.noreply.github.com>
2020-04-16 19:08:46 -0400

    Create ExerciseWhilePart4.java

---
+++ src/main/java/org/ExerciseWhilePart4.java
@@ -0,0 +1,19 @@
+package org;
+// Exercise 3.5.3:  ExerciseWhilePart4.java
+
+public class ExerciseWhilePart4 {
+    public static void main(String[] args) {
+
+    for (int i = 1; i <= 20; i++) {
+        System.out.print(i);
+
+        if (i % 5 == 0) {
+            System.out.println();
+        }
+        else{
+            System.out.print('\t');
+    }
+        
+}
+}
+}
